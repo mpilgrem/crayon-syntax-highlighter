8f7817695ba847bb87e47480cda606758e3a613a
Mike Pilgrem <mpilgrem@users.noreply.github.com>
2020-02-16 22:44:50 +0000

    Add Gutenberg functionality
    Based on commits at https://github.com/urvanov-ru/crayon-syntax-highlighter.

--- js/src/util.js
+++ js/src/util.js
@@ -78,6 +78,18 @@ var jQueryCrayon = jQuery;
             $.get(settings.ajaxurl, args, callback);
         };
 
+        /**
+         * @param {String} HTML representing any number of sibling elements
+         * @return {NodeList}
+         */
+        base.htmlToElements = function (html) {
+            return $.parseHTML(html, document, true);
+        };
+
+        base.htmlToFirstNode = function (html) {
+            return base.htmlToElements(html)[0];
+        };
+
         base.postAJAX = function (args, callback) {
             args.version = settings.version;
             $.post(settings.ajaxurl, args, callback);
